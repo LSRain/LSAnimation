be962c28dbbd30c2f0838003c9aa08016361b2ca
lsrain <315192042@qq.com>
2016-11-26 22:27:59 +0800

    project init

---
+++ LSAnimation/LSAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSAnimation
+//
+//  Created by WangBiao on 2016/11/26.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
